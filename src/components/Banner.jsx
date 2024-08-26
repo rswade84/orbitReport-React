dd36cc9416c08f746c4de754797b0734c9d3066c
Renaldo <renaldowade@gmail.com>
2024-08-26 18:45:34 -0500

    Orbit Report - All Test Pass - Final

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,6 @@
 import './styling.css';
 
 // NOTE: Banner renders the header and paragraph elements in the application...
-// NOTE: The Banner component is used in the App component...
 function Banner() {
   return (
     <div>
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,5 @@
 import './styling.css';
 
-// NOTE: The Table component receives 'sat' as a prop, which contains the satellite data to be displayed in the table...
-// NOTE: The Table component is used in the App component...
 const Table = ({ sat }) => {
   return (
     <table>
@@ -14,9 +12,6 @@ const Table = ({ sat }) => {
         </tr>
       </thead>
       <tbody>
-        {/* Maps over the 'sat' array (from app.jsx) to create a table row for each satellite in the data. */}
-        {/* key={data.id} is used to give each row a unique identifier for React's reconciliation process. */}
-        {/* (data) is just a placeholer for the current element being processed. */}
         {sat.map((data) => (
           <tr key={data.id}>
             <td>{data.name}</td>
